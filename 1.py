bb2508b6a14ce7f220606fe439bd0ca099108000
areal935 <areal935@gmail.com>
2022-04-14 18:17:11 +0300

    init commit

---
+++ 1.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+list1 = [i for i in range(20)]
+
+list2 = [i for i in range(5, 15)]
+aray1 = np.array(list1)
+arr_2d = np.array([list1, list2])
+np.arange(10, 1, -3)
+[round(x, 2) for x in np.linspace(1, 10, 50)]
+np.ones(10)
+np.zeros()
+np.zeros((4, 2), dtype=int)
+np.random.rand(3)
+arr_2d.shape
+np.arange(5)
